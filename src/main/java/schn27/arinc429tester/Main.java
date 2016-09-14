944a9c457a4e33144d1b755e9256de6231c0dcdd
schn27 <schn27@gmail.com>
2016-09-14 17:46:54 +0300

    call logger setup

--- src/main/java/schn27/arinc429tester/Main.java
+++ src/main/java/schn27/arinc429tester/Main.java
@@ -1,11 +1,16 @@
 package schn27.arinc429tester;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 import schn27.arinc429tester.ui.MainFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 	public static void main(String args[]) {
+		setupLogger();
 		setLookAndFeel();
 		MainFrame mainFrame = new MainFrame();
 		java.awt.EventQueue.invokeLater(() -> {mainFrame.setVisible(true);});
@@ -15,6 +20,14 @@ public class Main {
 		try {
 			UIManager.setLookAndFeel(UIManager.getInstalledLookAndFeels()[3].getClassName());
 		} catch (UnsupportedLookAndFeelException | ArrayIndexOutOfBoundsException | ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+		}
+	}
+	
+	private static void setupLogger() {
+		try {
+			LogManager.getLogManager().readConfiguration(Main.class.getResourceAsStream("/logging.properties"));
+		} catch (IOException | SecurityException ex) {
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 }
